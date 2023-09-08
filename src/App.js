be29b3ea89e4b79b9e4e6e4c550c68ab1f14e8e1
Dennis Fredborg <dennis_fred_z@hotmail.com>
2023-09-08 13:49:35 +0200

    iframe

--- src/App.js
+++ src/App.js
@@ -52,8 +52,11 @@ function App() {
     <div className="App">
       <iframe
         src='https://apps.powerapps.com/play/e/default-e15ec559-2613-43b3-90ec-9c684104b30d/a/5fc3b331-fa84-4c10-aa75-9cd2590ae54c?tenantId=e15ec559-2613-43b3-90ec-9c684104b30d&hint=f92488d5-cde3-49a9-89c4-5f044211ab7c&sourcetime=1694160116740&source=portal'
-        height={500}
-        width={500}
+        width="800"
+        height="600"
+        frameBorder="0"
+        allowFullScreen="true"
+        id="PowerApp"
       />
 
       <img src={nfc} className="App-logo" alt="logo" />
